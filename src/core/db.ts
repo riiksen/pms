c0886a707df1557e4a12e8850db3b0880e04a506
Mike <35075093+CerteXCm@users.noreply.github.com>
2024-04-08 18:27:13 +0200

    Add lucia auth

---
+++ src/core/db.ts
@@ -0,0 +1,28 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+
+// for migrations
+// const migrationClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db", { max: 1 });
+// migrate(drizzle(migrationClient), ...)
+
+// for query purposes
+const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db");
+export const db = drizzle(queryClient);
+
+export const userTable = pgTable("user", {
+	id: text("id").primaryKey()
+});
+
+export const sessionTable = pgTable("session", {
+	id: text("id").primaryKey(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp("expires_at", {
+		withTimezone: true,
+		mode: "date"
+	}).notNull()
+});
