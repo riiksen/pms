cdec14c2a1065a2cd2150e7da9a3ef813db20850
Mike <35075093+CerteXCm@users.noreply.github.com>
2024-04-09 15:53:15 +0200

    Format files with pretiier

--- src/core/auth.ts
+++ src/core/auth.ts
@@ -1,37 +1,37 @@
 import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { Lucia } from "lucia";
-import { users, sessions } from '@/schema';
 
-import { db } from './db';
+import { users, sessions } from "@/schema";
+
+import { db } from "./db";
 
 export const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
 
 export const lucia = new Lucia(adapter, {
-	sessionCookie: {
-		// this sets cookies with super long expiration
-		// since Next.js doesn't allow Lucia to extend cookie expiration when rendering pages
-		expires: false,
-		attributes: {
-			// set to `true` when using HTTPS
-			secure: process.env.NODE_ENV === "production"
-		}
-	},
-	getUserAttributes: (attributes) => {
-		return {
-			// attributes has the type of DatabaseUserAttributes
-			username: attributes.username
-		};
-	},
+  sessionCookie: {
+    // this sets cookies with super long expiration
+    // since Next.js doesn't allow Lucia to extend cookie expiration when rendering pages
+    expires: false,
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      // attributes has the type of DatabaseUserAttributes
+      username: attributes.username,
+    };
+  },
 });
 
-// IMPORTANT!
 declare module "lucia" {
-	interface Register {
-		Lucia: typeof lucia;
-		DatabaseUserAttributes: DatabaseUserAttributes;
-	}
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
 }
 
 interface DatabaseUserAttributes {
-	username: string;
+  username: string;
 }
--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,18 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-	id: text("id").primaryKey(),
-	username: text("username").notNull().unique(),
-	hashedPassword: text("hashed_password").notNull(),
+  id: text("id").primaryKey(),
+  username: text("username").notNull().unique(),
+  hashedPassword: text("hashed_password").notNull(),
 });
 
 export const sessions = pgTable("sessions", {
-	id: text("id").primaryKey(),
-	userId: text("user_id")
-		.notNull()
-		.references(() => users.id),
-	expiresAt: timestamp("expires_at", {
-		withTimezone: true,
-		mode: "date"
-	}).notNull()
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
 });
