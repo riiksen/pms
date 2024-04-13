22c508b698fe6766b4de80aed902fe77a81749b8
Mike <35075093+CerteXCm@users.noreply.github.com>
2024-04-13 15:51:41 +0200

    Extract the login action to a seperate file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,4 @@
-import { eq } from "drizzle-orm";
-import { Argon2id } from "oslo/password";
 import Link from "next/link";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -15,11 +11,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-import { lucia } from "@/core/auth";
-import { db } from "@/core/db";
-import { users } from "@/schema";
+import { login } from './actions';
 
-export function LoginForm() {
+export default function LoginForm() {
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
@@ -69,70 +63,3 @@ export function LoginForm() {
     </Card>
   );
 }
-
-const login = async (_: any, formData: FormData) => {
-  "use server";
-
-  const username = formData.get("username");
-  if (
-    typeof username !== "string" ||
-    username.length < 3 ||
-    username.length > 31 ||
-    !/^[a-z0-9_-]+$/.test(username)
-  ) {
-    return {
-      error: "Invalid username",
-    };
-  }
-  const password = formData.get("password");
-  if (
-    typeof password !== "string" ||
-    password.length < 6 ||
-    password.length > 255
-  ) {
-    return {
-      error: "Invalid password",
-    };
-  }
-
-  const existingUser = await db.query.users.findFirst({
-    where: eq(users.username, username.toLowerCase()),
-  });
-
-  if (!existingUser) {
-    // NOTE:
-    // Returning immediately allows malicious actors to figure out valid usernames from response times,
-    // allowing them to only focus on guessing passwords in brute-force attacks.
-    // As a preventive measure, you may want to hash passwords even for invalid usernames.
-    // However, valid usernames can be already be revealed with the signup page among other methods.
-    // It will also be much more resource intensive.
-    // Since protecting against this is non-trivial,
-    // it is crucial your implementation is protected against brute-force attacks with login throttling etc.
-    // If usernames are public, you may outright tell the user that the username is invalid.
-    return {
-      error: "Incorrect username or password",
-    };
-  }
-
-  const validPassword = await new Argon2id().verify(
-    existingUser.hashedPassword,
-    password,
-  );
-
-  if (!validPassword) {
-    return {
-      error: "Incorrect username or password",
-    };
-  }
-
-  const session = await lucia.createSession(existingUser.id, {});
-  const sessionCookie = lucia.createSessionCookie(session.id);
-
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes,
-  );
-
-  return redirect("/");
-};
