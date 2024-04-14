ba5026f294d9ca5b380cf0300b15095a65986209
Mike <35075093+CerteXCm@users.noreply.github.com>
2024-04-14 02:25:02 +0200

    Add a signup page

---
+++ src/app/signup/actions.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { generateId } from "lucia";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { Argon2id } from "oslo/password";
+
+import { lucia } from "@/core/auth";
+import { db } from "@/core/db";
+import { users } from "@/schema";
+
+interface ActionResult {
+  error: string;
+}
+
+export async function signup(_currentState: unknown, formData: FormData): Promise<ActionResult> {
+  const username = formData.get("username");
+  // username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
+  // keep in mind some database (e.g. mysql) are case insensitive
+  if (
+    typeof username !== "string" ||
+    username.length < 3 ||
+    username.length > 31 ||
+    !/^[a-z0-9_-]+$/.test(username)
+  ) {
+    return {
+      error: "Invalid username",
+    };
+  }
+  const password = formData.get("password");
+  if (
+    typeof password !== "string" ||
+    password.length < 6 ||
+    password.length > 255
+  ) {
+    return {
+      error: "Invalid password",
+    };
+  }
+
+  const hashedPassword = await new Argon2id().hash(password);
+  const userId = generateId(15);
+
+  // TODO: check if username is already used
+  const newUser = await db.insert(users).values({
+    id: userId,
+    username: username,
+    hashedPassword: hashedPassword,
+  });
+
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+
+  return redirect("/");
+}
